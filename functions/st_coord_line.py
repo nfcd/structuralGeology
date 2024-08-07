755f72a4651cbba38be7cfb03a2a89241e4c1e6a
nfcd <nestor.cardozo@uis.no>
2024-08-07 17:05:25 +0200

    Aug 7/24

---
+++ functions/st_coord_line.py
@@ -0,0 +1,35 @@
+import math
+from zero_twopi import zero_twopi
+
+def st_coord_line(trd,plg,stype):
+	"""
+	st_coord_line computes the coordinates of a line
+	in an equal angle or equal area stereonet of unit radius
+	
+	trd = trend of line
+	plg = plunge of line
+	stype = Stereonet type: 0 = equal angle, 1 = equal area
+	xp and yp: Coordinates of the line in the stereonet
+	
+	NOTE: trend and plunge should be entered in radians
+	
+	Python function translated from the Matlab function
+	StCoordLine in Allmendinger et al. (2012)
+	"""
+	# Take care of negative plunges
+	if plg < 0:
+		trd = zero_twopi(trd+math.pi)
+		plg = -plg
+	
+	# Equal angle stereonet
+	if stype == 0:
+		x = math.tan(math.pi/4 - plg/2)
+	# Equal area stereonet
+	elif stype == 1:
+		x = math.sqrt(2) * math.sin(math.pi/4 - plg/2)
+	
+	# Compute coordinates
+	xp = x * math.sin(trd)
+	yp = x * math.cos(trd)
+	
+	return xp, yp
